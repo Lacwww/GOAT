bd6ea43c7c986cc83036c258d9127cd6a3470e3f
tjdgu <tjdgu@DESKTOP-7TH0JT0>
2022-01-06 17:53:41 +0900

    22. 01. 06. kim tempplace model 추가

---
+++ goat/src/main/java/com/ch/goat/model/TempPlace.java
@@ -0,0 +1,26 @@
+package com.ch.goat.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class TempPlace {
+	private int temp_num;
+	private String temp_name;
+	private String temp_cate;
+	private String temp_addr;
+	private String temp_addrd;
+	private String temp_photo;
+	private String lat;
+	private String lng;
+	private Date reg_date;
+	private String temp_content;
+	private String temp_area;
+	private String temp_tag;
+	private String temp_areadetail;
+	private String temp_crud;
+	private int place_num;
+	private String del;
+	private int m_num;
+}
